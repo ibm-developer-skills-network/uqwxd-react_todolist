621b9f97b1a712f07ebb5290286f2498c5d954d9
Surjeet-SUJU <sujudonzz@gmail.com>
2024-06-24 13:08:00 +0530

    Updated redux with a lot more

--- react-redux-master/src/App.js
+++ react-redux-master/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import MainPanel from './components/MainPanel';
+import './App.css'
 
 function App() {
   return (
-      <div>
+      <div className="bigDiv">
         <MainPanel/>
       </div>
     );
